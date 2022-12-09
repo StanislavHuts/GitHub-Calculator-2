59022c528b245663e208770d7dfafded440a4512
StanislavHuts <stashuts@gmail.com>
2022-12-09 16:38:23 +0200

    Add int a and int b

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,13 @@
 package app;
 
 public class Main {
+    static int a = 8;
+    static int b = 2;
 
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
+
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
@@ -15,7 +16,6 @@ public class Main {
         System.out.println(e);
         int f = subtraction (a, b);
         System.out.println(f);
-
     }
 
     private static int add(int a, int b) {
