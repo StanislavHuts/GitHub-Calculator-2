22f9a2c823e7f5b35e313bcd9ab9fec07eb1c54f
StanislavHuts <stashuts@gmail.com>
2022-12-09 15:06:26 +0200

    Add subtraction and division return

--- src/app/Main.java
+++ src/app/Main.java
@@ -25,4 +25,10 @@ public class Main {
     private static int multiply(int a, int b) {
         return a * b;
     }
+    private static int division(int a, int b) {
+        return a / b;
+    }
+    private static int subtraction(int a, int b) {
+        return a - b;
+    }
 }
